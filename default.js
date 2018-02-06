dd025126e54ca2c7f8f17bb4ee7171f61170fdee
Veritas Homes <jrdfilter@gmail.com>
2018-02-06 12:45:34 -0800

    Updated Includes and 2 other files

--- default.js
+++ default.js
@@ -1,4 +1,28 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFrZWRvYnNvbiIsImEiOiJjajdzMHAwZ2Y0amhsMnFxdTgzZzRoa280In0.S2TkU2UgGv7dcsUsmy12cw';
 
+var offsetX = (-((window.innerWidth) / 4));
+
+function loadMap () {
+	var map = new mapboxgl.Map({
+		container:			'map',
+		minZoom:			9,
+		zoom:				10, //initial zoom
+		maxZoom:			16,
+		center: 			[-122.089348, 47.450844],
+		style: 'mapbox://styles/mapbox/satellite-v9?optimize=true',
+		interactive: 		true,
+		attributionControl: false,
+	});
+	document.getElementById('overlay').style.backgroundColor = "rgba(0,0,0,0.8)";
+	map.flyTo({
+		center: [-122.472398, 47.250291],
+		zoom: 15 ,
+		curve: 1.5,
+		speed: 0.75,
+		offset: [offsetX, 0],
+		pitch: 25
+	});
+}
 
 
 /* function hideContent() {
