1c7d4b164ada2be97d04b313207094e84ac70cd4
Veritas Homes <jrdfilter@gmail.com>
2018-04-24 11:53:51 -0700

    Updated Package Lock and 4 other files

--- default.js
+++ default.js
@@ -1,27 +1,39 @@
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFrZWRvYnNvbiIsImEiOiJjajdzMHAwZ2Y0amhsMnFxdTgzZzRoa280In0.S2TkU2UgGv7dcsUsmy12cw';
 
-var offsetX = (-((window.innerWidth) / 4));
+var offsetX	= -(((window.innerWidth) - 32) / 16);
+
+$ = {}
+$.minZoom	= 9;
+$.beginZoom = 9.55;
+$.maxZoom   = 13.5;
+$.pitch     = 50;
+$.offsetY	= 32;
+//$.markers 	= [];
+//$.office 	= null;
+$.speed 	= 2;
+$.curve 	= 1.5;
 
 function loadMap () {
 	var map = new mapboxgl.Map({
-		container:			'map',
-		minZoom:			9,
-		zoom:				10, //initial zoom
-		maxZoom:			16,
-		center: 			[-122.089348, 47.450844],
+		container: 'map',
+		minZoom: $.minZoom,
+		zoom: $.beginZoom,
+		maxZoom: $.maxZoom,
+		center: [-122.089348, 47.450844],
 		style: 'mapbox://styles/mapbox/satellite-v9?optimize=true',
-		interactive: 		true,
-		attributionControl: false,
+		interactive: true,
+		attributionControl: false
 	});
 	map.flyTo({
-		center: [-122.472398, 47.250291],
-		zoom: 15 ,
-		curve: 1.5,
-		speed: 0.75,
-		offset: [offsetX, 0],
-		pitch: 25
+		center: [-122.089348, 47.450844],
+		zoom: 	9.55,
+		curve: 	$.curve,
+		pitch: 	0,
+		offset: [offsetX, 32],
+		speed: 	$.speed
 	});
-	document.getElementById('overlay').style.backgroundColor = "rgba(0,0,0,0.8)";
+	//document.getElementById('overlay').style.backgroundColor = "rgba(0,0,0,0.6)";
 }
 
 
